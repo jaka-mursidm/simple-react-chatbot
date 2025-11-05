90ce3aa483a54ea9c8f4fe0f4f9677fd547a747c
Jaka Mursid M <jaka.mursidm@gmail.com>
2025-11-05 11:04:55 +0700

    Feat : Make a chat message into object and display into component

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,7 @@ function App() {
     <>
       <div className='max-w-2xl mx-auto pt-5'>
         <ChatInput />
-        <ChatMessage message="Hello chatbot!" sender="user" />
-        <ChatMessage message="Hello! How can i help you?😀" sender="robot" />
-        <ChatMessage message="Can you get met todays date?" sender="user" />
-        <ChatMessage message="Today is November 05?" sender="robot" />
+        <ChatMessage />
       </div>
     </>
   )
--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -1,17 +1,39 @@
-import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faRobot, faCircleUser } from '@fortawesome/free-solid-svg-icons'
-const ChatMessage = ({ message, sender }) => {
+const ChatMessage = () => {
+    const messages = [{
+        id: 1,
+        text: 'Hello chatbot!',
+        sender: 'user'
+    }, {
+        id: 2,
+        text: 'Hello! How can i help you?😀',
+        sender: 'robot'
+    }, {
+        id: 3,
+        text: 'Can you get met todays date?',
+        sender: 'user'
+    }, {
+        id: 4,
+        text: 'Today is November 05?',
+        sender: 'robot'
+    }
+    ];
 
     return (
         <>
-            <div className={`flex gap-1 items-center mb-4 ${sender === "user" ? "justify-end" : ""
-                }`}>
-                {sender === "robot" && (<FontAwesomeIcon icon={faRobot} className='text-4xl text-blue-500' />
-                )}
-                <div className='bg-slate-200 rounded py-2 px-3'>{message}</div>
-                {sender === "user" && (<FontAwesomeIcon icon={faCircleUser} className='text-4xl text-blue-500' />)}
-            </div>
+            {messages.map((message) => {
+                return (
+                    <div key={message.id} className={`flex gap-1 items-center mb-4 ${message.sender === "user" ? "justify-end" : ""
+                        }`}>
+                        {message.sender === "robot" && (<FontAwesomeIcon icon={faRobot} className='text-4xl text-blue-500' />
+                        )}
+                        <div className='bg-slate-200 rounded py-2 px-3'>{message.text}</div>
+                        {message.sender === "user" && (<FontAwesomeIcon icon={faCircleUser} className='text-4xl text-blue-500' />)}
+                    </div>
+                )
+            })}
+
         </>
     );
 }
