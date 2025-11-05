20edb223ac6ca251fce43815c8d643a34103a610
Jaka Mursid M <jaka.mursidm@gmail.com>
2025-11-05 10:41:24 +0700

    Style : add input message margin

--- src/components/ChatInput.jsx
+++ src/components/ChatInput.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ChatInput = () => {
     return (
-        <div className='flex gap-3'>
+        <div className='flex gap-3 mb-5'>
             <input type="text" class="flex-1 border border-gray-300 p-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Enter a message to chatbot" />
             <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Send</button>
         </div>
