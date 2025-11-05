9e06110426d8405c70787321224f05d9062c2023
Jaka Mursid M <jaka.mursidm@gmail.com>
2025-11-05 10:40:39 +0700

    Feat : add chat message element and sender condition

--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRobot, faCircleUser } from '@fortawesome/free-solid-svg-icons'
+const ChatMessage = ({ message, sender }) => {
 
-const ChatMessage = () => {
     return (
         <>
-
+            <div className={`flex gap-1 items-center mb-4 ${sender === "user" ? "justify-end" : ""
+                }`}>
+                {sender === "robot" && (<FontAwesomeIcon icon={faRobot} className='text-4xl text-blue-500' />
+                )}
+                <div className='bg-slate-200 rounded py-2 px-3'>{message}</div>
+                {sender === "user" && (<FontAwesomeIcon icon={faCircleUser} className='text-4xl text-blue-500' />)}
+            </div>
         </>
-    )
+    );
 }
 
 export default ChatMessage
