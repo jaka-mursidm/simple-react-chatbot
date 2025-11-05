0ab70758140d1f45db8dde058ad01057a50f5a84
Jaka Mursid M <jaka.mursidm@gmail.com>
2025-11-05 12:10:21 +0700

    Feat : Validation empty input

--- src/components/ChatInput.jsx
+++ src/components/ChatInput.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
 
+import { useState } from "react";
 const ChatInput = () => {
+    const [input, setInput] = useState("");
+    const sendHandler = () => {
+        alert(input);
+        setInput("");
+    };
     return (
         <div className='flex gap-3 mb-5'>
-            <input type="text" className="flex-1 border border-gray-300 p-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Enter a message to chatbot" />
-            <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Send</button>
+            <input type="text" className="flex-1 border border-gray-300 p-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Enter a message to chatbot" onChange={(e) => setInput(e.target.value)} />
+            <button className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer ${input.trim() === ""
+                    ? "bg-gray-400 cursor-not-allowed"
+                    : "bg-blue-500 hover:bg-blue-700 text-white"
+                }`} onClick={sendHandler} disabled={input.trim() === ""}>
+                Send</button>
         </div>
     )
 }
