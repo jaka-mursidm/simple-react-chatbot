7f3c58a9a805affdff47185d0ad60aa2fb798d5d
Jaka Mursid M <jaka.mursidm@gmail.com>
2025-11-06 09:26:08 +0700

    Style : Adjustment and remove favicon

--- src/components/ChatInput.jsx
+++ src/components/ChatInput.jsx
@@ -9,9 +9,9 @@ const ChatInput = ({ onSend }) => {
     return (
         <div className='flex gap-3 mb-5'>
             <input type="text" className="flex-1 border border-gray-300 p-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Enter a message to chatbot" onChange={(e) => setInput(e.target.value)} />
-            <button className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer ${input.trim() === ""
+            <button className={`bg-blue-500 text-white font-bold py-2 px-4 rounded cursor-pointer ${input.trim() === ""
                 ? "bg-gray-400 cursor-not-allowed"
-                : "bg-blue-500 hover:bg-blue-700 text-white"
+                : "hover:bg-blue-700 text-white"
                 }`} onClick={sendHandler} disabled={input.trim() === ""}>
                 Send</button>
         </div>
